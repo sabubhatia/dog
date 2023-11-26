43f18c8fbb7d0e5672faf5a5ef703c4a52a55cf7
Sarabjit Bhatia <sarabjitbhatia@Sarabjits-MacBook-Pro.local>
2023-11-26 11:41:49 +0800

    v1.0.0

--- dog.go
+++ dog.go
@@ -3,5 +3,5 @@ package dog
 import "strings"
 
 func WhenHeGrowsUp(s string) string {
-	return "When he grows up THE DOG says: " + strings.ToUpper(s)
+	return "When he grows up THE ADULT DOG says: " + strings.ToUpper(s)
 }
